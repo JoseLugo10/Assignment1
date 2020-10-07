3a9bcef074c1915f00bcd59a775707d690843c99
joselugo <jelugo3@asu.edu>
2020-10-07 16:24:04 -0700

    Version 2

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -1,33 +1,72 @@
 package cse360assignment02;
 
-// This Java file will be able to add, and subtract a given number from a number set to the variable total.
+/**
+ * The AddingMachine class acts as a calculator which when called upon can add, subtract, and print all of the operations
+ * done to the total integer variable.
+ */
 public class AddingMachine {
+
+    /**
+     * A blank main method to avoid compilation error. The actual program is utilized with a drivers program.
+     */
+    public static void main(String[] args)
+    {
+
+    }
+
+    // total will be used as a number to perform operations.
     private int total;
 
-    // The function AddingMachine allows for the operations to be made.
+    // The string variable operations will contain all of the operations done to the total variable. Will be used by the toString method.
+    private String operations = "0 ";
+
+    /**
+     * This is an AddingMachine method used for creating new AddingMachine objects in a driver's program.
+     */
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
     }
 
-    // Will return the value total to the user.
+    /**
+     * This method will return the value of total.
+     * @return the value of total
+     */
     public int getTotal () {
-        return 0;
+        return total;
     }
 
-    // Will add a given integer to the value of total.
+    /**
+     * An inputted number will be added onto the total variable.
+     * @param value the value being added to total
+     */
     public void add (int value) {
+        total += value;
+        operations += ("+ " + value + " ");
     }
 
-    // Will subtract a given integer from the value of total.
+    /**
+     * An inputted number will be subtracted from the value of total.
+     * @param value the value being subtracted from total
+     */
     public void subtract (int value) {
+        total -= value;
+        operations += ("- " + value + " ");
     }
 
-    // Basic toString() function which prints out the values.
+    /**
+     * A basic toString method which will print all of the operations done to the total integer.
+     * @return the string operations which contains the operations done to total
+     */
     public String toString () {
-        return "";
+        return operations;
     }
 
-    // Clears all values and restarts the calculations from the beginning.
+    /**
+     * Clears all of the operations and the values are reset.
+     */
     public void clear() {
+        total = 0;
+        System.out.println("Total has been reset to 0.");
+        operations = "0 ";
     }
 }
