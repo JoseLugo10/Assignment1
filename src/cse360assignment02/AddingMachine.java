6deff44fb53ba50503419ab4afdf819478a13ff7
Jose Lugo <joselugo@joses-mbp.3500.dhcp.asu.edu>
2020-10-07 10:04:05 -0700

    Initial commit

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,33 @@
+package cse360assignment02;
+
+// This Java file will be able to add, and subtract a given number from a number set to the variable total.
+public class AddingMachine {
+    private int total;
+
+    // The function AddingMachine allows for the operations to be made.
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    // Will return the value total to the user.
+    public int getTotal () {
+        return 0;
+    }
+
+    // Will add a given integer to the value of total.
+    public void add (int value) {
+    }
+
+    // Will subtract a given integer from the value of total.
+    public void subtract (int value) {
+    }
+
+    // Basic toString() function which prints out the values.
+    public String toString () {
+        return "";
+    }
+
+    // Clears all values and restarts the calculations from the beginning.
+    public void clear() {
+    }
+}
